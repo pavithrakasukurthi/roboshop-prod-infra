48846675af3ab7961992e8c6c7a8a21eda0a0c68
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-11 21:17:26 +0530

    ter-ans

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -18,19 +18,19 @@ data "aws_ami" "ami_id" {
     }
 }
 
-data "aws_ssm_parameter" "sg_id" {
+data "aws_ssm_parameter" "mongodb_sg_id" {
     name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
 
-data "aws_ssm_parameter" "sg_id" {
+data "aws_ssm_parameter" "redis_sg_id" {
     name = "/${var.project}/${var.environment}/redis_sg_id"
 }
 
-data "aws_ssm_parameter" "sg_id" {
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
     name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 
-data "aws_ssm_parameter" "sg_id" {
+data "aws_ssm_parameter" "mysql_sg_id" {
     name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
