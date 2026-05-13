3f50c686bde29f3b929f7f1aaf3b311483f3c4cb
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-13 12:31:51 +0530

    terraform

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,5 +1,5 @@
 variable "sg_names" {
-    default = ["catalogue", "redis", "bastion", "mongodb", "mysql", "rabbitmq", "cart", "user", "shipping", "payment", backend_alb]
+    default = ["catalogue", "redis", "bastion", "mongodb", "mysql", "rabbitmq", "cart", "user", "shipping", "payment", "backend_alb"]
 }
 
 variable "project" {
