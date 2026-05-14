01777465da4ceced18d02e6aea63fae4359ce646
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-14 12:59:34 +0530

    terraform

---
+++ 80-frontend-alb/locals.tf
@@ -0,0 +1,11 @@
+locals {
+    frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
+    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    frontend_alb_certificate_arn = data.aws_ssm_parameter.frontend_alb_certificate_arn.value
+    common_name = "${var.project}-${var.environment}"
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = true
+    }
+}
