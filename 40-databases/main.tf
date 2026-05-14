041464a428954ecb961800149abadc9849291745
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-14 14:13:19 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -34,7 +34,7 @@ resource "terraform_data" "mongodb" {
 }
 resource "aws_route53_record" "mongodb" {
     zone_id = var.zone_id
-    name = mongodb-${var.environment}.${var.domain_name}
+    name = "mongodb-${var.environment}.${var.domain_name}"
     type = "A"
     ttl = 1
     records = [aws_instance.mongodb.private_ip]
@@ -124,7 +124,7 @@ resource "terraform_data" "mysql" {
 }
 resource "aws_route53_record" "mysql" {
     zone_id = var.zone_id
-    name = mysql-${var.environment}.${var.domain_name}
+    name = "mysql-${var.environment}.${var.domain_name}"
     type = "A"
     ttl = 1
     records = [aws_instance.mysql.private_ip]
@@ -168,7 +168,7 @@ resource "terraform_data" "rabbitmq" {
 }
 resource "aws_route53_record" "rabbitmq" {
     zone_id = var.zone_id
-    name = rabbitmq-${var.environment}.${var.domain_name}
+    name = "rabbitmq-${var.environment}.${var.domain_name}"
     type = "A"
     ttl = 1
     records = [aws_instance.rabbitmq.private_ip]
