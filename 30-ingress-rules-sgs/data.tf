e7a8dbd9cf2123395033934f2dd1aa3b09485493
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-14 14:04:09 +0530

    terraform

--- 30-ingress-rules-sgs/data.tf
+++ 30-ingress-rules-sgs/data.tf
@@ -37,3 +37,7 @@ data "aws_ssm_parameter" "shipping_sg_id" {
 data "aws_ssm_parameter" "payment_sg_id" {
     name = "/${var.project}/${var.environment}/payment_sg_id"
 }
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+}
