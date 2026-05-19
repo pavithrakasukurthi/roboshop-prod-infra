35c6f1d978d2d380c4c0cdfeb08f8cd98fdf2531
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-19 17:28:14 +0530

    terraform

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -32,5 +32,5 @@ resource "aws_route53_record" "roboshop" {
 
 resource "aws_acm_certificate_validation" "roboshop" {
     certificate_arn = aws_acm_certificate.roboshop.arn
-    validation_record_fqdn = [for record in aws_route53_record.roboshop : record.fqdn]
+    validation_record_fqdns = [for record in aws_route53_record.roboshop : record.fqdn]
 }
