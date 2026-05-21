67315817bcee701e4f3ed72378a7d980d2bdc647
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-21 11:36:20 +0530

    terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
     common_name = "${var.project}-${var.environment}"
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 
     common_tags = {
         Project = var.project
