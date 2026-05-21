37901a6171183dfdfb75909ad3f47f50eb861a64
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-21 17:47:49 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -89,6 +89,12 @@ resource "aws_instance" "mysql" {
     instance_type = var.instance_type
     vpc_security_group_ids = [local.mysql_sg_id]
     subnet_id = local.database_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
+
+    resource "aws_iam_instance_profile" "mysql" {
+    name = "mysql"
+    role = "MysqlSSMParameterRead"
+    }
 
     tags = merge(
         local.common_tags,
