2ce01de842dcde194d26d804f4a7f32f0ef4ae95
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-23 14:34:02 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -21,7 +21,7 @@ resource "terraform_data" "catalogue" {
         type = "ssh"
         user = "ec2-user"
         password = "DevOps321"
-        host = aws_instance.cata.private_ip
+        host = aws_instance.catalogue.private_ip
     }
 
     provisioner "file" {
