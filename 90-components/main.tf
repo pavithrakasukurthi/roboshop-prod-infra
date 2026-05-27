c601a7c3f14386cd4dc6aa782ec4a6d38ce334b6
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-27 19:36:50 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -4,6 +4,7 @@ module "component" {
      count = length(var.component)
      project = var.project
      environment = var.environment
+     component = var.component[count.index]
      env = var.env
      instance_type = var.instance_type
      domain_name = var.domain_name
