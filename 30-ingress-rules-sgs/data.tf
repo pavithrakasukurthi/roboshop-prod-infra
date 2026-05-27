ac1789cabd9ebce55675b426fcb09fa30cd39a31
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-27 19:04:54 +0530

    terraform

--- 30-ingress-rules-sgs/data.tf
+++ 30-ingress-rules-sgs/data.tf
@@ -45,3 +45,7 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
 data "aws_ssm_parameter" "frontend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
+
+data "aws_ssm_parameter" "frontend_sg_id" {
+    name = "/${var.project}/${var.environment}/frontend_sg_id"
+}
--- 30-ingress-rules-sgs/locals.tf
+++ 30-ingress-rules-sgs/locals.tf
@@ -11,4 +11,5 @@ locals {
     shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
 }
