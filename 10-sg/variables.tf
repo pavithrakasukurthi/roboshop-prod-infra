fbcad68152806fb471fc5e4119bc2d47869b1981
Pavithra <pavithrakasukurthi00@gmail.com>
2026-06-01 11:21:04 +0530

    terraform

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,5 +1,5 @@
 variable "sg_names" {
-    default = ["catalogue", "redis", "bastion", "mongodb", "mysql", "rabbitmq", "cart", "user", "shipping", "payment", "backend_alb", "frontend_alb", "frontend"]
+    default = ["catalogue", "redis", "bastion", "mongodb", "mysql", "rabbitmq", "cart", "user", "shipping", "payment", "backend_alb", "frontend_alb", "frontend", "openvpn"]
 }
 
 variable "project" {
---
+++ 21-openvpn/locals.tf
@@ -0,0 +1,11 @@
+locals {
+    ami_id = data.aws_ami.openvpn.id
+    openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
+    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    common_name = "${var.project}-${var.environment}"
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = true
+    }
+}
